730060fb4761d1a25916b8120ad3d62e9583e3f4
JeremyLewis5 <jeremy.lewis@codeimmersives.com>
2021-03-04 14:55:31 -0600

    update after solution

--- main.js
+++ main.js
@@ -9,20 +9,35 @@ function numberDoubler(num) {
  }
  //console.log('Output num:', outputNum);
 
- function stringRepeater(str, num) {
-     let string = str;
-     let num1 = 4;
-      while (string <= 10) {
-        //console.log(string);  
-        string = str * num1;
+ function stringRepeater(str) {
+    //  let string = str;
+    //  let num1 = 4;
+    //   while (string <= 10) {
+    //     //console.log(string);  
+    //     string = str * num1;
         
-      }
-     return string;
- }
+    //   }
+    //  return string;
+let outputStr = str;
+while(outputStr.length < 10) {
+    outputStr += str;
+    
+} 
+return outputStr
+
+
+
+}
+
+
 
 
 function makeDivisible(x, y) {
-    // Your code here
+    let outputX = x;
+    while(outputX % y !== 0){
+        outputX ++
+    }
+return outputX
 }
 
 module.exports = {
